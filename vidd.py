cc58128b768bacc140b13537e13b0b96dd4e92df
Ryan Tanaka <ryanyt@hawaii.edu>
2021-05-05 17:41:24 -0700

    added metrics

--- vidd.py
+++ vidd.py
@@ -2,12 +2,21 @@
 import sys
 import pickle
 import struct
+import logging as log
+from pathlib import Path
+from statistics import mean
+from time import perf_counter as tpf
 from socket import *
 import cv2 as cv
 
+def to_mb(b: int) -> float:
+    return b / (1 << 20)
+
 if __name__=="__main__":
+    VIDEO_FILE = "surf.mp4"
+
     # read video into memory, frame by frame
-    cap = cv.VideoCapture("surf.mp4")
+    cap = cv.VideoCapture(VIDEO_FILE)
     frames = list()
 
     while cap.isOpened():
@@ -15,10 +24,12 @@ if __name__=="__main__":
         frames.append(frame)
 
         if not ret:
-            print("Can't receive frame (stream end?).")
+            print("Can't receive frame (possibly stream end?).")
             break
     
     print("captured {} frames".format(len(frames)))
+    print("average frame size: {:0.3f} Mb".format(to_mb(mean([sys.getsizeof(f) for f in frames]))))
+    print("video size: {:0.3f} Mb".format(to_mb(Path(VIDEO_FILE).stat().st_size)))
 
     s = socket(AF_INET, SOCK_STREAM)
     s.bind(("", 9090))
@@ -32,17 +43,28 @@ if __name__=="__main__":
 
         # send frames
         for frame in frames:
+            start = tpf()
+
+            # serialize frame
             frame_p = pickle.dumps(frame)
 
             # first value in binary string is unsigned long long ('Q') that
             # specifies the length of the following data segment followed
             # by the actual message itself
-            payload = struct.pack("!Q", len(frame_p)) + frame_p
+            payload = struct.pack("Q", len(frame_p)) + frame_p
 
             # send binary string 
-            print("sending payload of size: {}".format(sys.getsizeof(payload)))
-            client.send(payload)
+
+            # will buffer frame and ensure entire payload is sent
+            # send() returns # bytes sent, and may send less than payload, so 
+            # using sendall isntead
+            client.sendall(payload)
+            end = tpf()
+
+            print("sent payload of size: {size:0.3f} Mb; {dur:0.3f} seconds".format(
+                size=to_mb(sys.getsizeof(payload)),
+                dur=end - start
+            ))
 
         client.close()
 
-    
