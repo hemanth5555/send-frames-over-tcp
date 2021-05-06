1241737d767573ba0970276a9cc9a9de63574a95
Ryan Tanaka <ryanyt@hawaii.edu>
2021-05-05 17:41:39 -0700

    added metrics; added frame by frame recv

--- client_player.py
+++ client_player.py
@@ -2,7 +2,7 @@
 import pickle
 import struct
 from socket import *
-
+from time import perf_counter as tpf
 import cv2 as cv
 
 if __name__=="__main__":
@@ -10,7 +10,7 @@ if __name__=="__main__":
     s.connect(("", 9090))
     print("connected to server...")
 
-    # save movie in memory
+    # buffer to save frame
     data = bytearray()
 
     # size of number used to designate payload size
@@ -19,48 +19,61 @@ if __name__=="__main__":
     
     # read in frames
     print("receiving data from server")
-    while len(data) < payload_size_size:
-        received = s.recv(4096)
-
-        # connection has been closed
-        if len(received) == 0:
-            break
-
-        data.extend(received)
-
-
-
-    s.close()
-    
-    print("len of data: {}".format(len(data)))
-    # play all at the end for now 
-    print("playing back frames")
-    i = 0
     while True:
-        if i + payload_size_size - 1 > len(data):
-            print("got out")
-            break
+        # 1 iteration = receiving 1 frame (1 + a little more in some cases)
+        start = tpf()
 
-        payload_size = int.from_bytes(
-                    data[i:i+payload_size_size], 
-                    byteorder="big",
-                    signed=False
-                )
+        # first figure out the size of a payload, then build up
+        while len(data) < payload_size_size:
+            received = s.recv(4096)
 
-        i += payload_size_size 
-        frame = pickle.loads(bytes(data[i:i+payload_size]))
+            # connection has been closed
+            if len(received) == 0:
+                break
 
-        i += payload_size
+            data.extend(received)
 
-        cv.imshow("frame", frame) 
+
+        # get payload info; which is in the first 8 bytes of the payload
+        packed_msg_size = data[:payload_size_size]
+        
+        # get the message content (everything after the first 8 bytes), which is our frame
+        data = data[payload_size_size:]
+
+        # get payload size so we know how much data to read in before we have a full frame
+        msg_size = struct.unpack("Q",packed_msg_size)[0]
+
+        # get the actual frame (we know from msg_size how much bytes to recv)
+        # might read a little extra bytes, but will handle that 
+        while len(data) < msg_size:
+            data.extend(s.recv(4096))
+
+        # frame will be data all the way up to msg_size
+        frame = data[:msg_size]
+        
+        # overwrite data, with left over "data" which includes the next frame
+        data = data[msg_size:]
+        end = tpf()
+
+        # get recv time before displaying image (likely faster than
+        # shown on the server side because data has already arrived buffered
+        # before calling recv
+        print("recv frame time: {:0.3f} seconds".format(end - start))
+
+        cv.imshow("frame", pickle.loads(frame))
         if cv.waitKey(40) == ord("q"):
             print("you pressed 'q' and we will exit")
             break
+
+    s.close()
 
 '''
 case 1: have incomplete payload_size_size (means I dont' have payload)
 case 2: have complete payload_size_size
         case 2.1: have incomplete payload
         case 2.2: have complete payload 
+
+
+reading frame by frame adopted from https://stackoverflow.com/questions/49084143/opencv-live-stream-video-over-socket-in-python-3
 '''
 
